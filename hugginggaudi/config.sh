ec0dd621f87d116b29a3c83386b0f1b018584b29
Rory Finnegan <rory@denvrdata.com>
2024-09-16 16:01:11 -0700

    Fixed the compose file healthchecks work and added password auth to the caddy reverse proxy.

--- hugginggaudi/config.sh
+++ hugginggaudi/config.sh
@@ -4,17 +4,38 @@ set -e
 
 ROOT=$(pwd)
 
+echo 'Pulling down docker images'
+time sudo docker compose pull
+
+plaintext=""
+prompt="Enter an admin password: "
+while IFS= read -p "$prompt" -r -s -n 1 char
+do
+    # Exit when they hit enter
+    if [[ $char == $'\0' ]]
+    then
+        break
+    fi
+
+    # Print a '*' for each input character
+    prompt="*"
+
+    # Update the saved key and the count increment
+    plaintext+="$char"
+done
+passwd_hash=$(sudo docker run -t caddy:latest caddy hash-password -p "$plaintext")
+
 IP=$(curl ifconfig.me)
 mkdir -p .config/caddy
 echo "Writing .config/caddy/Caddyfile"
 tee .config/caddy/Caddyfile <<EOF
 $IP.nip.io {
     reverse_proxy chatui:3000
+    basicauth {
+        admin $passwd_hash
+    }
 }
 EOF
-
-echo 'Pulling down docker images'
-time sudo docker compose pull
 
 mkdir -p data/mongo
 mkdir -p data/tgi
@@ -28,4 +49,3 @@ echo 'Starting docker services'
 time sudo docker compose up -d
 
 echo "Configuration complete. Open ${IP}.nip.io in your browser."
-
